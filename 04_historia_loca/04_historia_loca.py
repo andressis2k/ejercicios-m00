9f3f56789c61e8b273babf09a07df19abc6e8f85
Andrés Luis <andressis2000@gmail.com>
2025-07-06 16:14:16 +0200

    Actualizacion

--- 04_historia_loca/04_historia_loca.py
+++ 04_historia_loca/04_historia_loca.py
@@ -5,10 +5,10 @@ adjetivo = input("Introduce un adjetivo: ")
 
 print("1 - Al parque de atracciones")
 print("2 - A la playa")
-eleccion = input("¿A donde quiéres que vaya nuestro protagonista?")
-if eleccion =="1":
-    texto = "{} decidió ir al parque de atracciones y una vez allí empezó a {} {}. De repente, todo el mundo se fue de la playa y él se quedó {}"
-if eleccion =="2":
+eleccion = input("¿A donde quiéres que vaya nuestro protagonista? ")
+if eleccion == "1":
+    texto = "{} decidió ir al parque de atracciones y una vez allí empezó a {} {}. De repente, todo el mundo se fue del parque y él se quedó {}"
+elif eleccion == "2":
     texto = "{} fue a la playa abarrotada y decidió {} muy {}. La gente pensaba que era muy {} y todos se le quedaron mirando"
 else:
     texto = "{} no quiso ir a ningún sitio ese día, por lo que decidió quedarse en casa a {} {}. Muy {} por su parte."
